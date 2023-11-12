0e0e5ebd0a67751b42de1df84992d81ff96ffce5
MarcVillarroya <marcvillarroyafernandez@gmail.com>
2023-11-12 23:53:25 +0100

    google drive hecho!!!!

--- multerConfig.js
+++ multerConfig.js
@@ -1,38 +1,54 @@
+const { google } = require('googleapis');
 const multer = require('multer');
-const path = require('path');
+const stream = require('stream');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    if (file.fieldname === 'video') {
-      cb(null, path.join(__dirname, 'public/video_productos'));
-    } else {
-      cb(null, path.join(__dirname, 'public/img_productos'));
-    }
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  },
+// Configura la autenticación de Google utilizando las variables de entorno
+const auth = new google.auth.GoogleAuth({
+    credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
+    },
+    scopes: ['https://www.googleapis.com/auth/drive.file'],
 });
 
-const fileFilter = (req, file, cb) => {
-  if (file.fieldname === 'video') {
-    if (file.mimetype === 'video/mp4' || file.mimetype === 'video/webm') {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  } else {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/webp') {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
+const driveService = google.drive({ version: 'v3', auth });
+
+const uploadFileToGoogleDrive = async (fileObject, isVideo) => {
+    const bufferStream = new stream.PassThrough();
+    bufferStream.end(fileObject.buffer);
+
+    const folderId = isVideo ? '13GpXRx0KOVynwvcU4RdFm35fnyYt4MrL' : '1ldv5248POuRX4O39RF7snLksx8Uj7snh'; // Reemplaza con tus IDs de carpetas
+
+    const { data } = await driveService.files.create({
+        media: {
+            mimeType: fileObject.mimetype,
+            body: bufferStream,
+        },
+        requestBody: {
+            name: fileObject.originalname,
+            parents: [folderId],
+        },
+        fields: 'id',
+    });
+
+    return data.id;
+};
+
+const deleteFileFromGoogleDrive = async (fileId) => {
+  try {
+      await driveService.files.delete({
+          fileId: fileId,
+      });
+  } catch (error) {
+      console.error('Error al eliminar archivo de Google Drive:', error);
+      throw error; // Lanzar el error para manejarlo en el llamador
   }
 };
 
-const upload = multer({ storage: storage, fileFilter: fileFilter });
+const upload = multer({
+    storage: multer.memoryStorage(),
+});
 
-module.exports = {
-  upload,
-};
+module.exports = { upload, uploadFileToGoogleDrive, deleteFileFromGoogleDrive };
+
 
